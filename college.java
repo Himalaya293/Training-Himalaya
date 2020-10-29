7c42a1ffbad48722d86a9339bc0dcd65a4508d8b
Himalaya293 <73583283+Himalaya293@users.noreply.github.com>
2020-10-29 16:36:26 +0530

    Add files via upload

--- college.java
+++ college.java
@@ -1,27 +1,20 @@
+import java.util.Scanner;
 class college{
-String department;
-int students;
-int sections;
-int feez;
-String classteachername;
-String collegename;
+void listofcollege(String cllge){
+System.out.println("cllge is"+cllge);
 }
-class variables{
+void listofcollege(String cllge1,String cllge2){
+System.out.println("cllge is"+cllge1+"and"+cllge2);
+}
+void listofcollege(String cllge1,String cllge2,String cllge3){
+System.out.println("cllge is"+cllge1+"and"+cllge2+" "+cllge3);
+}
+}
+class customer{
 public static void main(String []args)
 {
-college cllg=new college();
-cllg.department="ECE";
-cllg.students=450;
-cllg.sections=4;
-cllg.feez=500000;
-//cllg.classteachername="Hemalatha";
-cllg.collegename="MRECW";
-System.out.println("---------------------college Details-------------");
-System.out.println("cllg.department="+cllg.department);
-System.out.println("cllg.students="+cllg.students);
-System.out.println("cllg.sections="+cllg.sections);
-System.out.println("cllg.feez="+cllg.feez);
-System.out.println("cllg.classteachername="+cllg.classteachername);
-System.out.println("cllg.collegename="+cllg.collegename);
+college cg=new college();
+Scanner sc=new Scanner(System.in);
+cg.listofcollege(sc.next());
 }
 }
