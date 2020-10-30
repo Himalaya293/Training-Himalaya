928b8cca8e6e5a92bbf3fe53b6f31dcda9b88fab
Himalaya293 <73583283+Himalaya293@users.noreply.github.com>
2020-10-30 16:55:12 +0530

    Add files via upload

--- abs.java
+++ abs.java
@@ -1,34 +1,17 @@
 abstract class abs{
-abstract void cost();
-abstract void millage();
-void start(){
+abstract void start();
+abs(){
 System.out.println("start with a key");
 }
 }
-class audi extends abs{
-void cost(){
-System.out.println("Audi cost is 50 lakhs");
-}
-void millage(){
-System.out.println("Audi millage is 10km");
-}
-}
 class bwm extends abs{
-void cost(){
+void start(){
 System.out.println("BWM cost is 40 lakhs");
-}
-void millage(){
-System.out.println("BWM millage is 10km");
 }
 }
 class abstraction{
 public static void main(String []args){
-abs x=new audi();
-x.cost();
-x.millage();
+abs x=new bwm();
 x.start();
-x=new bwm();
-x.cost();
-x.millage();
 }
 }
