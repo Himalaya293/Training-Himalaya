fbb3b882c58462361047e873ff6b66d31b3e9b32
CVCORP <CVCORP@DESKTOP-N56TG3N>
2021-01-19 12:33:18 +0530

    First Commit

---
+++ Hybernatejoined/src/com/motivity/Cheque.java
@@ -0,0 +1,14 @@
+package com.motivity;
+
+public class Cheque extends Payment {
+
+	private String chequetype;
+
+	public String getChequetype() {
+		return chequetype;
+	}
+
+	public void setChequetype(String chequetype) {
+		this.chequetype = chequetype;
+	}
+}
