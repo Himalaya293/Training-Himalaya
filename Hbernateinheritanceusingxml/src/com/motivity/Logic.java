11789af42e5c913aef1de65e1bbbce6c9ff2acb1
CVCORP <CVCORP@DESKTOP-N56TG3N>
2021-01-19 12:31:49 +0530

    First Commit

--- Hbernateinheritanceusingxml/src/com/motivity/Logic.java
+++ Hbernateinheritanceusingxml/src/com/motivity/Logic.java
@@ -1,7 +1,5 @@
 package com.motivity;
 
-import java.util.HashSet;
-import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,32 +9,27 @@ public class Logic {
 
 	public static void main(String[] args) {
 		
-
+		
 		Configuration cf=new Configuration();
 		cf.configure("hibernate.cfg.xml");
 		SessionFactory sf=cf.buildSessionFactory();
         Session se=sf.openSession();
         
         Transaction tx=se.beginTransaction();
-		 
-        Student std=new Student();
-        std.setSid(104);
-        std.setSname("seema");
-        std.setAddress("gnp");
         
-        Laptop lp=new Laptop();
-        lp.setLid(104);
-        lp.setLname("lenovo");
-        lp.setLcode("AXP52");
+        Cheque cd=new Cheque();
+        cd.setPaymentid(101);
+        cd.setAmount(50000);
+        cd.setChequetype("ICICI");
+       
         
         
-        Set<Student> set=new HashSet<Student>();
-        set.add(std);
-        lp.setStudent(set);
-        
-        se.save(lp);
+        se.save(cd);
         tx.commit();
         se.close();
+        
+        
 	}
+
 
 }
